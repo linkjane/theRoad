edfe8bdbc40190d38d40ce2e21f0ab0274579b06
MuteKen <916969890@qq.com>
2016-06-25 06:47:48 -0400

    commit

--- app/route/index.server.route.js
+++ app/route/index.server.route.js
@@ -1,4 +1,3 @@
-
 module.exports = function (app) {
     app.get('/', function (req, res) {
         res.render('index', {
@@ -6,4 +5,31 @@ module.exports = function (app) {
         });
         
     })
+	app.get('/interface', function(req, res){
+		var token = 'iloveyouforver';
+		var signature = req.query.signature;
+		var timestamp = req.query.timestamp;
+		var echostr = req.query.echostr;
+		var nonce = req.query.nonce;
+		
+		var oriArray = [];
+		oriArray[0] = nonce;
+		oriArray[1] = timestamp;
+		oriArray[2] = token;
+		oriArray.sort();
+		
+		var original = oriArray.join('');
+		var jsSHA = require('jssha');
+		var shaObj = new jsSHA(original, 'TEXT');
+		console.log(1);
+		var scyptoString = shaObj.getHash('SHA-1', 'HEX');
+		console.log(2);
+		if (signature === scyptoString) {
+			console.log('confirm and send echo back');
+			res.end(echostr);
+		} else {
+			console.log('failed');
+		}
+
+	} )
 }
